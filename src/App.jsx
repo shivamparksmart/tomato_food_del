b522382ec2e6c330e1d214d0b6ca9543f9c3e964
shivamparksmart <shivam.anand936@gmail.com>
2025-01-16 11:48:09 +0530

    SEO enabled

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,26 @@ import Cart from "./pages/Cart/Cart";
 import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 import Footer from "./components/Footer/Footer";
 import LoginPopup from "./components/LoginPopup/LoginPopup";
-import { useState } from "react";
+import { Profiler, useState } from "react";
 import Verify from "./pages/Verify/Verify";
 import MyOrders from "./pages/MyOrders/MyOrders";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Helmet } from "react-helmet";
+
+function onRenderCallback(
+  id, // The "id" prop of the Profiler tree that just rendered
+  phase, // Either "mount" or "update"
+  actualDuration, // Time spent rendering the Profiler subtree
+  baseDuration, // Estimated time to render the subtree without memoization
+  startTime, // When React started rendering this update
+  commitTime, // When React committed this update
+  interactions // Set of interactions belonging to this update
+) {
+  console.log(`Profiler [${id}]`);
+  console.log(`Phase: ${phase}`);
+  console.log(`Actual Duration: ${actualDuration}ms`);
+}
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
@@ -20,14 +35,23 @@ const App = () => {
       <div className="app">
         <Navbar setShowLogin={setShowLogin} />
         <ToastContainer />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/order" element={<PlaceOrder />} />
-          <Route path="/verify" element={<Verify />} />
-          <Route path="/myorders" element={<MyOrders />} />
-        </Routes>
+        <Profiler id="RoutesProfiler" onRender={onRenderCallback}>
+          <Helmet>
+            <title>Tomato.</title>
+            <meta
+              name="description"
+              content="Discover the best cuisines, foods, and dining experiences in India with Tomato."
+            />
+          </Helmet>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/order" element={<PlaceOrder />} />
+            <Route path="/verify" element={<Verify />} />
+            <Route path="/myorders" element={<MyOrders />} />
+          </Routes>
+        </Profiler>
       </div>
       <Footer />
     </>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,12 +4,24 @@ import Header from "../../components/Header/Header";
 import "./Home.css";
 import FoodDisplay from "../../components/FoodDisplay/FoodDisplay";
 import AppDownload from "../../components/AppDownload/AppDownload";
+import { Helmet } from "react-helmet";
 
 const Home = () => {
   const [category, setCategory] = useState("All");
 
   return (
     <div>
+      <Helmet>
+        <title>Tomato. - Order your favourite food now !</title>
+        <meta
+          name="description"
+          content="Select from best food options available"
+        />
+        <meta
+          name="keywords"
+          content="restaurants, food, cuisines, dining, best restaurants near me, affordable food places, famous street foods, pizza, sandwiches"
+        />
+      </Helmet>
       <Header />
       <ExploreMenu category={category} setCategory={setCategory} />
       <FoodDisplay category={category} />
